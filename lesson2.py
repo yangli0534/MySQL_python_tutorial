8e4f128aec24d83b638a3b23f97660493d676673
yangli0534 <yangli0534@yahoo.com>
2021-04-27 22:24:55 +0800

    second commit

---
+++ lesson2.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Leon
+@time: 2021-04-27
+
+"""
+
+from getpass import getpass
+from mysql.connector import connect, Error
+
+movie_id = input("Enter movie id: ")
+reviewer_id = input("Enter reviewer id: ")
+new_rating = input("Enter new rating: ")
+update_query = """
+UPDATE
+    ratings
+SET
+    rating = "%s"
+WHERE
+    movie_id = "%s" AND reviewer_id = "%s";
+
+SELECT *
+FROM ratings
+WHERE
+    movie_id = "%s" AND reviewer_id = "%s"
+""" % (
+    new_rating,
+    movie_id,
+    reviewer_id,
+    movie_id,
+    reviewer_id,
+)
+
+try:
+    with connect(
+        host="localhost",
+        user="root",
+        password="oppaha89",
+        database="online_movie_rating",
+    ) as connection:
+        with connection.cursor() as cursor:
+            for result in cursor.execute(update_query, multi=True):
+                if result.with_rows:
+                    print(result.fetchall())
+            connection.commit()
+except Error as e:
+    print(e)
